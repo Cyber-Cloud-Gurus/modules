70eb4967bf1136cb2498cab3752e2c979d72ea9a
Farhad Nadri <farhad.nadri@cybercloudgurus.com>
2023-11-23 10:37:33 -0500

    update

--- mggroup/mggroup.tf
+++ mggroup/mggroup.tf
@@ -22,7 +22,6 @@ locals {
 }
 resource "azurerm_management_group" "region-bu" {
   for_each = local.reg_all
-  for_each =
   display_name = "MG-${var.shortcompanyname}-${each.value.region}-${each.value.region_bu}"
   parent_management_group_id = azurerm_management_group.region-mg[each.key].id
   depends_on = [
