b8def7e4b04e7dfca0b0005ea687e97feab7791f
Farhad Nadri <farhad.nadri@cybercloudgurus.com>
2023-11-28 12:37:59 -0500

    update module

--- mggroup/mggroup.tf
+++ mggroup/mggroup.tf
@@ -37,14 +37,14 @@ resource "azurerm_management_group" "corp-mg" {
     ]
   }
  /*resource "azurerm_management_group" "bu-mg" {
-  for_each = toset(var.corp)
+  for_each = toset(var.bu)
   display_name = "MG-${var.shortcompanyname}-${each.value}"
-  parent_management_group_id = azurerm_management_group.corp-mg[each.key].id
+  parent_management_group_id = azurerm_management_group.corp-mg.id
   depends_on = [
     azurerm_management_group.corp-mg
   ]
 }*/
-resource "azurerm_management_group" "platform-mg" {
+/*resource "azurerm_management_group" "platform-mg" {
     for_each = toset(var.region)
     display_name = "MG-${var.shortcompanyname}-${var.platform}-${each.value}"
     parent_management_group_id = azurerm_management_group.region-mg[each.key].id
@@ -53,7 +53,7 @@ resource "azurerm_management_group" "platform-mg" {
     ]
   
 }
-/*resource "azurerm_management_group" "plat_bu" {
+resource "azurerm_management_group" "plat_bu" {
   for_each = toset(var.platform)
     display_name = "mg-${var.shortcompanyname}-${each.value}"
     parent_management_group_id = azurerm_management_group.platform-mg[each.key].id
